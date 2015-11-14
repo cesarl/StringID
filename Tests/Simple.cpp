2c149956cb4db8e9a2f5fa14d1fa13dcd39c5136
Cesar Leblic <cesar.leblic@gmail.com>
2015-11-14 19:05:29 +0100

    Simple file for simple tests

---
+++ Tests/Simple.cpp
@@ -0,0 +1,16 @@
+
+
+
+StringID("easy");
+StringID( "easy" );
+StringID ("easy");
+StringID  ("easy");
+StringID(  "easy"  );
+
+ StringID("easy");
+ StringID( "easy" );
+WRONGStringID ("easy");
+WRONG_StringID("easy");
+WRONG,StringID("easy");
+
+
