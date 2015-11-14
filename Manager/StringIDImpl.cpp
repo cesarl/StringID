48c31e5391c0160f353deb8f47315d416000b280
Cesar Leblic <cesar.leblic@gmail.com>
2015-11-14 18:36:23 +0100

    Simple header and cpp added to Manager to help lib configuration

---
+++ Manager/StringIDImpl.cpp
@@ -0,0 +1,3 @@
+#define STRINGID_IMPL
+
+#include "StringIDImpl.hpp"
---
+++ Manager/StringIDImpl.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#define STRINGID_SUPPORT_STD_STRING 1
+#define STRINGID_DEBUG_ENABLED      1
+#define STRINGID_CHECK_COLLISION    1
+#define STRINGID_DB_MAP_INCLUDE     <unordered_map>
+#define STRINGID_DB_MAP             std::unordered_map
+
+#include "../src/StringID.hpp"
