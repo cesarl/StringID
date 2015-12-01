b8b31459e9f70ef6df9b129f4bd5dab815f99864
Cesar Leblic <cesar.leblic@gmail.com>
2015-12-01 19:54:11 +0100

    Test updated

--- Tests/Simple.cpp
+++ Tests/Simple.cpp
@@ -19,5 +19,8 @@ StringID(  "Wrong"  ,  123 123 123);
 WRONGStringID ("easy" ,,, 123);
 WRONG_StringID("easy" ,123);
 WRONG,StringID("easy" , 0x1233221);
-std::make_pair(std::make_pair(GL_FLOAT_VEC4, StringID("colorA", 0x77f5c18e246c6638)), LAMBDA_FUNCTION{ vertices.set_data<glm::vec4>(data.colors,            StringID("colorB", 0x77f5c18e246c6638)); })
+std::make_pair(std::make_pair(GL_FLOAT_VEC4, StringID("colorA", 0x42fe5d87e4321f9b)), LAMBDA_FUNCTION{ vertices.set_data<glm::vec4>(data.colors,            StringID("colorB", 0x42fe5e87e432214e)); })
 std::make_pair(std::make_pair(GL_FLOAT_VEC4, StringID("colorA")), LAMBDA_FUNCTION{ vertices.set_data<glm::vec4>(data.colors,            StringID("colorB")); })
+std::make_pair(std::make_pair(GL_FLOAT_VEC4, StringID("color", 0x77f5c18e246c6638)), LAMBDA_FUNCTION{ vertices.set_data<glm::vec4>(data.colors,            StringID("color", 0x77f5c18e246c6638)); })
+std::make_pair(std::make_pair(GL_FLOAT_VEC4, StringID("color")), LAMBDA_FUNCTION{ vertices.set_data<glm::vec4>(data.colors,            StringID("color")); })
+
